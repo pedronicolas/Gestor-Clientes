d54c28b394625f2d541419812eab884ddf66381b
pedronicolas <pedro.nicolas.perez96@gmail.com>
2019-12-01 09:59:52 +0100

    created interface Cliente

--- src/app/data-models.ts
+++ src/app/data-models.ts
@@ -1,9 +1,18 @@
 export interface Pedidos {
+    Productos: Array<Producto>,
+    PrecioTotal: number,
+
 }
 
-export interface Cliente{
-   Nombre: string,
-   CIF: string,
-   FechaUltPedido: Date,
-        
+export interface Producto {
+    Codigo: string,
+    Descripcion: string,
+}
+
+export interface Cliente {
+    Nombre: string,
+    CIF: string,
+    FechaUltPedido: Date,
+    Pedidos:Array<Pedidos>,
+    // TODO: Ver que más necesita un cliente:      
 }
